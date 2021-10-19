36a5a0f1088573321278b45a20867a647aa3fc52
Emma Lynn <emmalynnn97@gmail.com>
2021-10-19 11:12:32 -0700

    add in the thing that will hold the pics

--- src/Desktop.tsx
+++ src/Desktop.tsx
@@ -5,12 +5,16 @@ import {ReactComponent as Notes} from './Notes.svg'
 import {ReactComponent as Music} from './Music.svg'
 import {ReactComponent as Settings} from './Messages.svg'
 import {ReactComponent as Folder} from './Folder.svg'
+import Gallery from 'react-photo-gallery'
+import Image1 from './Image1.png'
+
+
 type DesktopProps = {
     
 }
 
 const Desktop = ({}: DesktopProps) => (
-  
+        
         <div style={{
             backgroundImage: `url(${bg})`,
             backgroundSize: 'cover',
@@ -29,6 +33,16 @@ const Desktop = ({}: DesktopProps) => (
                 <Folder/>
                 <span>Photos</span>
             </a>
+            <div style={{
+                position:'absolute',
+                width:'100%',
+                height:'100%',
+                display:'flex',
+                justifyContent:'center',
+                alignItems:'flex-start'
+            }}>
+<div style={{height:'60%', width:'90%', background:'rgb(30,30,30)', position:'relative', top:50}}></div>
+            </div>
             <div style={{padding:'0 14px', backgroundColor:'rgba(255, 255, 255, 0.1)', width:'auto', height:75, position:'absolute', bottom: 50, display:'flex',justifyContent:'center', alignItems:'center', gap:7}}>
                 <Messages/>
                 <Calendar/>
