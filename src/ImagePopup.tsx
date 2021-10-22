826d49fc35b42bac8c82e200e7d6fdba722ae4c4
Emma Lynn <emmalynnn97@gmail.com>
2021-10-21 17:45:35 -0700

    add lightbox

--- src/ImagePopup.tsx
+++ src/ImagePopup.tsx
@@ -1,11 +1,24 @@
+import React, { useState, useCallback } from "react"
 import header from './Header.png'
 import Gallery from 'react-photo-gallery'
 import {photos} from './photos'
+import Carousel, { Modal, ModalGateway } from "react-images";
+import type {ViewType} from 'react-images'
 interface Props {
     isActive?: boolean
 }
 const ImagePopup = ({isActive}: Props) => {
     let display = isActive ? 'flex' : 'none'
+    const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
     return (
       
         <div className='image-popup-modal' style={{
@@ -19,7 +32,20 @@ const ImagePopup = ({isActive}: Props) => {
         }}>
             <div className='popup-container' style={{ height: '80%', width: '90%', background: 'rgb(30,30,30)', position: 'relative', top: 50 }}>
                 <img style={{width:'100%'}} src={header}/>
-                    <Gallery targetRowHeight={200} photos={photos}/>
+                    <Gallery onClick={openLightbox} targetRowHeight={200} photos={photos}/>
+                    <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                srcset: x.src
+              })) as any}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
             </div>
         </div>
         
