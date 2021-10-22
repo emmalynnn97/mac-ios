0fd7ab90b2d98a721f78016392337ca9b1d064f4
Emma Lynn <emmalynnn97@gmail.com>
2021-10-22 14:51:18 -0700

    add right photos

--- src/photos.js
+++ src/photos.js
@@ -1,72 +1,78 @@
 export const photos = [
     {
-        src: "https://i.imgur.com/8NnN91a.png",
+        src: "https://i.imgur.com/aebnvdq.jpg",
         width: 552,
         height: 596,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/wc0HXNh.png",
+        src: "https://i.imgur.com/aDbh6HJ.jpg",
         width: 397,
         height: 397,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/RtqovxG.png",
+        src: "https://i.imgur.com/LXuA8i4.jpg",
         width: 197,
         height: 197,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/jImz9WN.png",
+        src: "https://i.imgur.com/lJIpeGN.jpg",
         width: 197,
         height: 197,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/z4VCMWG.png",
+        src: "https://i.imgur.com/fvBrz72.jpg",
         width: 303,
         height: 184,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/3nmL4eS.png",
+        src: "https://i.imgur.com/ibAt29I.jpg",
         width: 304,
         height: 184,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/ehbtFZP.png",
+        src: "https://i.imgur.com/EvKTsUN.jpg",
         width: 166,
         height: 184,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/zCSlvok.png",
+        src: "https://i.imgur.com/w2qOGka.jpg",
         width: 166,
         height: 184,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/aVVm9Y0.png",
+        src: "https://i.imgur.com/OiziBde.jpg",
         width: 527,
         height: 522,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/giMVdOW.png",
+        src: "https://i.imgur.com/yRyfFfN.jpg",
         width: 415,
         height: 342,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/xJPJxGU.png",
+        src: "https://i.imgur.com/OlLGn1i.jpg",
         width: 217,
         height: 171,
         className: 'popup-image'
     },
     {
-        src: "https://i.imgur.com/BWCrNhZ.png",
+        src: "https://i.imgur.com/hEqEl6s.jpg",
+        width: 203,
+        height: 168,
+        className: 'popup-image'
+    },
+    {
+        src: "https://i.imgur.com/Y1n93jb.jpg",
         width: 203,
         height: 168,
         className: 'popup-image'
